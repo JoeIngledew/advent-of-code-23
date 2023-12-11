0b90ac01753500e7a31cd8233411fd93613f06d7
Joe Ingledew <joe.ingledew@ofqual.gov.uk>
2023-12-11 15:42:51 +0000

    day 10 part 1, using somebody else's code - still unsure I understand how to solve this problem, definitely no idea how to solve part 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,130 +1,129 @@
-// use hashbrown::{HashMap, HashSet};
-// use itertools::Itertools;
+// NOTE: Not my own work - this is copied from the github link below in an attempt to understand how to solve this problem.
+// I do not think this attempt was successful, I still do not properly understand it.
+
+// link: https://github.com/agausmann/puzzles/blob/52b3d7ca715d8b51d9ba3d9a595a496a449d10e9/adventofcode/aoc2023/src/bin/day10.rs
+
+use std::{cmp::Reverse, collections::BinaryHeap};
+
+use hashbrown::HashSet;
+use itertools::Itertools;
 
 advent_of_code::solution!(10);
 
-// #[derive(Debug, Eq, PartialEq, PartialOrd, Ord, Hash, Clone, Copy)]
-// struct Point {
-//     x: usize,
-//     y: usize
-// }
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
+struct MapTile {
+    north: bool,
+    south: bool,
+    east: bool,
+    west: bool,
+    start: bool,
+}
 
-// #[derive(Debug)]
-// struct MapCell {
-//     coords: Point,
-//     east: bool,
-//     west: bool,
-//     north: bool,
-//     south: bool,
-//     start: bool
-// }
+impl From<char> for MapTile {
+    fn from(value: char) -> Self {
+        match value {
+            '|' => Self {
+                north: true,
+                south: true,
+                ..Default::default()
+            },
+            '-' => Self {
+                east: true,
+                west: true,
+                ..Default::default()
+            },
+            'L' => Self {
+                north: true,
+                east: true,
+                ..Default::default()
+            },
+            'J' => Self {
+                north: true,
+                west: true,
+                ..Default::default()
+            },
+            'F' => Self {
+                south: true,
+                east: true,
+                ..Default::default()
+            },
+            '7' => Self {
+                south: true,
+                west: true,
+                ..Default::default()
+            },
+            'S' => Self {
+                start: true,
+                ..Default::default()
+            },
+            _ => Default::default(),
+        }
+    }
+}
 
-// impl MapCell {
-//     fn from_map_char(x: usize, y: usize, char: char) -> Option<Self> {
-//         let p = Point { x, y };
-//         match char {
-//             '|' => Some(MapCell { coords: p, east: false, west: false, north: true, south: true, start: false }),
-//             '-' => Some(MapCell { coords: p, east: true, west: true, north: false, south: false, start: false }),
-//             'L' => Some(MapCell { coords: p, east: true, west: false, north: true, south: false, start: false }),
-//             'J' => Some(MapCell { coords: p, east: false, west: true, north: true, south: false, start: false }),
-//             '7' => Some(MapCell { coords: p, east: false, west: true, north: false, south: true, start: false }),
-//             'F' => Some(MapCell { coords: p, east: true, west: false, north: false, south: true, start: false }),
-//             'S' => Some(MapCell { coords: p, east: false, west: false, north: false, south: false, start: true }),
-//             _ => None
-//         }
-//     }
-// }
+fn get_map(input: &str) -> (Vec<Vec<MapTile>>, (usize, usize)) {
+    let mut start: Option<(usize, usize)> = None;
+    let mut map = input
+        .lines()
+        .enumerate()
+        .map(|(y, l)| {
+            l.chars()
+                .enumerate()
+                .map(|(x, c)| {
+                    if c == 'S' {
+                        // horrid mutation of a variable in a map, just to avoid having to find the start later
+                        // this has to be y,x because of the way I'm constructing the grid (ie. sideways???)
+                        start = Some((y, x));
+                    }
+                    MapTile::from(c)
+                })
+                .collect_vec()
+        })
+        .collect_vec();
 
-// fn parse_map(input: &str) -> HashMap<Point, MapCell> {
-//     let mut map: HashMap<Point, MapCell> = HashMap::new();
-//     input.lines().enumerate().for_each(|(y, l)| {
-//         l.chars().enumerate().for_each(|(x, c)| {
-//             let p = Point { x, y };
-//             match MapCell::from_map_char(x, y, c) {
-//                 Some(m) => { map.insert(p, m); },
-//                 None => ()
-//             }
-//         });
-//     });
-//     map
-// }
+    let start = start.unwrap();
+    map[start.0][start.1].north = start.0 > 0 && map[start.0 - 1][start.1].south;
+    map[start.0][start.1].south = start.0 < map.len() - 1 && map[start.0 + 1][start.1].north;
+    map[start.0][start.1].west = start.1 > 0 && map[start.0][start.1 - 1].east;
+    map[start.0][start.1].east = start.1 < map[start.0].len() - 1 && map[start.0][start.1 + 1].west;
 
-// enum Direction {
-//     North,
-//     South,
-//     East,
-//     West
-// }
+    (map, start)
+}
 
-// impl Direction {
-//     fn opposite(&self) -> Self {
-//         match self {
-//             Direction::North => Direction::South,
-//             Direction::East => Direction::West,
-//             Direction::South => Direction::North,
-//             Direction::West => Direction::East
-//         }
-//     }
-// }
+fn get_far_point_distance(map: &[Vec<MapTile>], start: (usize, usize)) -> usize {
+    let mut dist = 0;
+    let mut heap = BinaryHeap::new();
+    let mut visits: HashSet<(usize, usize)> = HashSet::new();
+    heap.push((Reverse(0), start));
 
-// fn get_connections(curr_cell: &MapCell, map: &HashMap<Point, MapCell>) -> Vec<Point> {
-//     let mut possibilities: Vec<(Point, Direction)> = Vec::new();
-//     let x = curr_cell.coords.x;
-//     let y = curr_cell.coords.y;
-//     if x != 0 {
-//         possibilities.push((Point { x: x-1, y }, Direction::West));
-//     }
-//     if y != 0 {
-//         possibilities.push((Point { x, y: y-1 }, Direction::North));
-//     }
-//     possibilities.push((Point { x: x+1, y }, Direction::East));
-//     possibilities.push((Point { x, y: y+1 }, Direction::South));
+    while let Some((Reverse(k), (i, j))) = heap.pop() {
+        if !visits.insert((i, j)) {
+            continue;
+        }
+        dist = dist.max(k);
+        let c = map[i][j];
+        if c.north {
+            heap.push((Reverse(k + 1), (i - 1, j)));
+        }
+        if c.south {
+            heap.push((Reverse(k + 1), (i + 1, j)));
+        }
+        if c.west {
+            heap.push((Reverse(k + 1), (i, j - 1)));
+        }
+        if c.east {
+            heap.push((Reverse(k + 1), (i, j + 1)));
+        }
+    }
 
-//     possibilities.iter().filter_map(|(p, d)| {
-//         map.get(p).and_then(|v| {
-//             match d.opposite() {
-//                 Direction::East => if v.east && (curr_cell.west || curr_cell.start) { Some(*p) } else { None },
-//                 Direction::West => if v.west && (curr_cell.east || curr_cell.start) { Some(*p) } else { None },
-//                 Direction::North => if v.north && (curr_cell.south || curr_cell.start) { Some(*p) } else { None },
-//                 Direction::South => if v.south && (curr_cell.north || curr_cell.start) { Some(*p) } else { None },
-//             }
-//         })
-//     }).collect()
-// }
-
-// fn draw_loop(map: &HashMap<Point, MapCell>) -> Vec<&MapCell> {
-//     let start = map.values().find(|v| v.start).unwrap();
-//     let mut next_cells = get_connections(start, map);
-//     let mut loop_points: HashSet<Point> = HashSet::new();
-//     loop_points.insert(start.coords);
-//     while !next_cells.is_empty() {
-//         let mut inserted = 0;
-//         next_cells.iter().for_each(|c| {
-//             if loop_points.insert(*c) {
-//                 inserted += 1;
-//             }
-//         });
-//         if inserted != 0 {
-//             next_cells = next_cells.iter().flat_map(|c| {
-//                 match map.get(c) {
-//                     Some(v) => get_connections(v, map),
-//                     None => Vec::new()
-//                 }
-//             }).collect();
-//         } else {
-//             next_cells = Vec::new();
-//         }
-//     }
-//     loop_points.iter().filter_map(|p| map.get(p)).collect_vec()
-// }
+    dist
+}
 
 // way too slow. todo!()
-pub fn part_one(_input: &str) -> Option<usize> {
-    None
-    // let map = parse_map(input);
-    // let loop_points = draw_loop(&map);
-    // let half_len = loop_points.len() / 2;
-    // Some(half_len)
+pub fn part_one(input: &str) -> Option<usize> {
+    let (map, start) = get_map(input);
+    let res = get_far_point_distance(&map, start);
+    Some(res)
 }
 
 pub fn part_two(_input: &str) -> Option<u32> {
@@ -138,7 +137,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(8));
     }
 
     #[test]
